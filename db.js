bcc57cd493f8ec45d846c060a4caec6444a71b07
Bartol Buljubasic <bartol.buljubasic.91@gmail.com>
2026-06-27 14:16:20 -0400

    Commit rezervacije timezone issue fix

--- db.js
+++ db.js
@@ -2,18 +2,21 @@ const mysql = require('mysql2');
 
 //require('dotenv').config();
 
-const pool = mysql.createPool({  
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,   
-    waitForConnections: true, 
-    connectionLimit: 10,  
-    queueLimit: 0,   
-    ssl: {       
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    ssl: {
         rejectUnauthorized: false
-    }
+    },
+    // --- DODANO ZA RJEŠAVANJE VREMENSKE ZONE ---
+    timezone: 'Z',
+    dateStrings: true
 });
 
 const promisePool = pool.promise();
